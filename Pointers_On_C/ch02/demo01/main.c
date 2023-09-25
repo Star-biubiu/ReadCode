9b6735feca607f750545120a7a2fbb3997994943
Star-biubiu <zwb11020707@163.com>
2023-09-25 15:58:31 +0800

    demo01 多文件编译

---
+++ Pointers_On_C/ch02/demo01/main.c
@@ -0,0 +1,19 @@
+/*
+    编写一个程序，它由3个函数组成，每个函数分别保存在一个单独的源文件中。
+    函数increment接受一个整型参数，它的返回值是该参数的值加1,increment函数应该位于文件increment.c中。
+    第2个函数称为negate，它也接受一个整型参数，它的返回值是该参数的负值（例如，如果参数是25，函数返回-25；如果参数是612，函数返回-612）。
+    最后一个函数是main，保存于文件main.c中，它分别用参数10, 0和-10调用另外两个函数，并打印出结果。
+*/
+
+#include <stdio.h>
+#include "increment.c"
+#include "negate.c"
+
+int main(void)
+{
+    printf("increment(10) = %d\tnegate(10) = %d\n", increment(10), negate(10));
+    printf("increment(0) = %d\tnegate(0) = %d\n", increment(0), negate(0));
+    printf("increment(-10) = %d\tnegate(-10) = %d\n", increment(-10), negate(-10));
+
+    return 0;
+}
