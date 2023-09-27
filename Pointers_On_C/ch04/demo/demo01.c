c45302195e7775f4731118ffc9bae6a0f774314c
Star-biubiu <zwb11020707@163.com>
2023-09-27 13:00:50 +0800

    demo01 平方根

--- Pointers_On_C/ch04/demo/demo01.c
+++ Pointers_On_C/ch04/demo/demo01.c
@@ -5,25 +5,49 @@
     编写一个程序，读入一个值，计算并打印出它的平方根。如果你将所有的近似值都打印出来，你会发现这种方法获得准确结果的速度有多快。原则上，这种计算
     可以永远进行下去，它会不断产生更加精确的结果。但在实际中，由于浮点变量的精度限制，程序无法一直计算下去。当某个近似值与前一个近似值相等时，你
     就可以让程序停止继续计算了
+
+    a_(i+1) = (a_i + n / a_i) / 2 -> a_(i) = (a_(i-1) + n / a_(i-1)) / 2
 */
 
 #include <stdio.h>
 
+#define EPS 1e-8
+
 int main(void)
 {
-    int n;
-    double current_n, next_n;
+    // a_(i) = (a_(i-1) + n / a_(i-1)) / 2  这个算法好像只能算n大于等于1
+    double n, current_n, last_n;
     puts("Input a value:");
-    scanf("%d", &n);
-    current_n = n;
+    scanf("%lf", &n);
+    if (n < 0)
+        perror("input error");
 
-    while (1) {
-        next_n = (current_n + n / current_n) / 2;
-        if (current_n - next_n < 0.00000001)
-            break;
-        current_n = next_n;
+    if (n == 0)
+        printf("value's square = %.8lf\n", n);
+    else {
+        current_n = last_n = n;
+        
+        do {
+            last_n = current_n;
+            current_n = (last_n + (n / last_n)) / 2;
+            printf("value's square = %.8lf\n", current_n);
+        } while (last_n - current_n >= EPS);
     }
-    printf("value's square = %.8f", current_n);
 
     return 0;
 }
+
+/*
+    // a_(i+1) = (a_i + n / a_i) / 2
+
+    double n, current_n, next_n;
+    current_n = n;
+
+    while (1) {
+        next_n = (current_n + (n / current_n)) / 2;
+        if (current_n - next_n < EPS)
+            break;
+        current_n = next_n;
+        printf("value's square = %.8lf\n", current_n);
+    }
+*/
