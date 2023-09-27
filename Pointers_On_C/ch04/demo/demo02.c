e2b8568072f62e269d45f02fe706992f722389d2
Star-biubiu <zwb11020707@163.com>
2023-09-27 13:26:56 +0800

    demo02 质数

--- Pointers_On_C/ch04/demo/demo02.c
+++ Pointers_On_C/ch04/demo/demo02.c
@@ -1,6 +1,11 @@
 /*
     一个整数如果只能被它本身和1整除，它就被称为质数(prime)。
     请编写一个程序，打印出1～100之间的所有质数。
+    
+    提示：最小的因子不可能大于它的平方根
+    如果i能被j整除那么存在k，使 i = j * k。
+    如果 j 大于 i 的平方根，那么 k 必定小于 i 的平方根 -> 所以此时的j是包含k的，这样就重复计算了
+    因此，只要计算小于 i / j的部分
 */
 
 #include <stdio.h>
@@ -17,12 +22,13 @@ int main(void)
 }
 
 void cout_prime(int num) {
-    int tmp = num - 1;
-    while (tmp > 1) {
-        if (num % tmp == 0)
+    int flag = 1;
+    for (int tmp = 2; tmp <= num / tmp; tmp++)
+        if (num % tmp == 0) {
+            flag = 0;
             break;
-        tmp--;
-    }
-    if (tmp == 1) 
+        }
+        
+    if (flag) 
         printf("%d ", num);
 }
