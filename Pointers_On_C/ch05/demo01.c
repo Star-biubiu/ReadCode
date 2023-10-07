163796beafb2ea4c0cbf8ae3d06c4dd34a89c14b
Star-biubiu <zwb11020707@163.com>
2023-10-07 19:54:10 +0800

    demo01 大小写字母转换

--- Pointers_On_C/ch05/demo01.c
+++ Pointers_On_C/ch05/demo01.c
@@ -11,7 +11,7 @@ int main(void)
 
     while ( (ch = getchar()) != EOF) {
         if (ch >= 'A' && ch <= 'Z')
-            ch += 32;
+            ch += 32;  // <==>  ch += 'a' - 'A';
         putchar(ch);
     }
 
